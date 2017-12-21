058f2cb699ffd5d7e5448dbdefb148b741cc4b24
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-12-21 13:37:34 +0100

    changed to .net standard 2.0 implemented first test case added basic add functionality

---
+++ src/UConfig.Core/Upgrader.cs
@@ -0,0 +1,70 @@
+﻿namespace UConfig.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Dynamic;
+    using System.Xml.Linq;
+
+    public class Upgrader
+    {
+        private readonly XElement tree;
+
+        public Upgrader(XElement tree)
+        {
+            this.tree = tree;
+            mapping = new List<dynamic>();
+        }
+
+        private List<dynamic> mapping;
+
+
+        public void Apply(XElement configTree)
+        {
+            foreach (var entry in mapping)
+            {
+                expandoToXML(entry, "");
+            }
+        }
+
+        private XElement expandoToXML(dynamic node, String nodeName)
+        {
+            XElement xmlNode;
+            if (string.IsNullOrEmpty(nodeName))
+            {
+                xmlNode = tree;
+            }
+            else
+            {
+                xmlNode = new XElement(nodeName);
+            }
+
+
+            foreach (var property in (IDictionary<String, Object>)node)
+            {
+
+                if (property.Value.GetType() == typeof(ExpandoObject))
+
+                    xmlNode.Add(expandoToXML(property.Value, property.Key));
+
+                else if (property.Value.GetType() == typeof(List<dynamic>))
+
+                    foreach (var element in (List<dynamic>)property.Value)
+
+                        xmlNode.Add(expandoToXML(element, property.Key));
+
+                else
+
+                    xmlNode.Add(new XElement(property.Key, property.Value));
+
+            }
+
+            return xmlNode;
+
+        }
+
+        public void AddEntry(dynamic xpath)
+        {
+            this.mapping.Add(xpath);
+        }
+    }
+}
--- src/UConfig.Test/UpdateTests.cs
+++ src/UConfig.Test/UpdateTests.cs
@@ -1,19 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UConfig.Test
+﻿namespace UConfig.Test
 {
+    using System.Dynamic;
+    using System.IO;
+    using System.Xml.Linq;
+    using Core;
+    using FluentAssertions;
     using Xunit;
 
     public class Class1
     {
         [Fact]
-        public void When_I_upgrade_from_v1_to_v2_Then_the_config_should_contain_Added_setting_keys()
+        public void When_I_add_a_setting_to_the_config_Then_it_is_added_to_the_xml_document()
         {
+            XElement oldTree = new XElement("configuration",
+                new XElement("ExampleString", "test"),
+                new XElement("ExampleNumber", 2)
+            );
 
+
+            Upgrader upgrader = new Upgrader(oldTree);
+            dynamic configuration = new ExpandoObject();
+            configuration.AddedNumber = "3";
+            upgrader.AddEntry(configuration);
+            upgrader.Apply(oldTree);
+
+            oldTree.Element("AddedNumber").Value.Should().Be("3");
         }
     }
 }
+
+
